8d9b78311860d2bad58e75f31c1889796d6399b6
Adam Gąsowski <adam.gasowski@gander.pl>
2023-01-24 17:49:06 +0100

    replace by template with bootstrap

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,15 +2,16 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     #[Route('/')]
-    public function index(): Response
+    #[Template]
+    public function index(): array
     {
-        return new Response('OK?');
+        return [];
     }
 }
