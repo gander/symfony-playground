f790b59e395eaea21e45aaa2cf00802798602b46
Adam Gąsowski <adam.gasowski@gander.pl>
2023-01-25 12:05:26 +0100

    prepare for GitHub Actions

--- rector.php
+++ rector.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
@@ -18,7 +19,10 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
 
     // define sets of rules
-    //    $rectorConfig->sets([
-    //        LevelSetList::UP_TO_PHP_81
-    //    ]);
+        $rectorConfig->sets([
+            LevelSetList::UP_TO_PHP_82,
+            SetList::CODING_STYLE,
+            SetList::CODE_QUALITY,
+            SetList::DEAD_CODE,
+        ]);
 };
