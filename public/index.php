3960a9c1ecde5f856379a6feb7e4af6b0a1bc584
Adam Gąsowski <adam.gasowski@gander.pl>
2023-01-25 14:06:32 +0100

    some commit

--- public/index.php
+++ public/index.php
@@ -4,4 +4,10 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-return static fn(array $context) => new Kernel($context['APP_ENV'], (bool)$context['APP_DEBUG']);
+/**
+ * @param array $context
+ * @return Kernel
+ */
+return static function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool)$context['APP_DEBUG']);
+};
